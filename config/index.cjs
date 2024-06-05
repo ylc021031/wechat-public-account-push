5a7d15c9cd982dd912ba19caedf111942e55da6a
ylc021031 <140991673+ylc021031@users.noreply.github.com>
2024-06-05 11:28:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oxHIJ6a3JChoTl2OPhHbgZQ_lEME',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ldiNR5R5ufTcm5cKRZstTPJkpcpQh8rX4Zb6gytL3RM',
+      useTemplateId: 'SNRdn48T7MRqC_3CGbblIkGaYTDHZeNyaveGxpvsOdo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-31',
       festivals: [
